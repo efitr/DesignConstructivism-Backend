64e9258c69d2694d7c881d5103fad1596284b513
Egon Fiedler <efiedler93@gmail.com>
2018-10-19 03:27:55 -0700

    basic setup

---
+++ app.js
@@ -0,0 +1,31 @@
+
+const express = require('express');
+const app = express();
+// const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+// app.set('view engine', 'handlebars');
+app.use(methodOverride('_method'));
+
+module.exports = app;
+
+// const charities = require('./controllers/charities')(app)
+// const opinions = require('./controllers/opinions')(app)
+// const users = require('./controllers/users')(app);
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/charity-tracker', {useNewUrlParser: true});
+
+
+
+
+
+// app.listen(3000, () => {
+//   console.log('App listening on port 3000!')
+// })
+// Added after heroku
+const port = process.env.PORT || 3000;
+app.listen(port);
